21ea806e58cc5da0f43e34b1fe517f65d2d047c1
Ivy Sham <ivysham888@gmail.com>
2025-07-10 22:00:55 +0800

    Initial Commit

---
+++ 250710_Navigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  250710_Navigation
+//
+//  Created by Ivy Sham on 10/7/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 250710_Navigation/_50710_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  _50710_NavigationApp.swift
+//  250710_Navigation
+//
+//  Created by Ivy Sham on 10/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct _50710_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
